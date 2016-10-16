046f7c11a9f5456c0c680dad17b2ef149404340a
hardyj1 <hardyj1@seattleu.edu>
2016-10-16 22:36:37 +0000

    modified config.php file with Flickr key

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-$API_KEY = 'Put your API key here'; // This will be your Flickr-issued API Key.
-$PAGE_TITLE = 'Kittens and Rainbows'; // This should be a user-friendly page title.
-$SEARCH_TAGS = 'kitten,rainbow'; // This must be a comma-separated list
+$API_KEY = '9fe0bff7000bd81980657d505cb838a6'; // This will be your Flickr-issued API Key.
+$PAGE_TITLE = 'Sports and cars'; // This should be a user-friendly page title.
+$SEARCH_TAGS = 'sports,cars'; // This must be a comma-separated list
 
 ?>
